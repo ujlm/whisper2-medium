d92758aeaf93e135680f94e58d2fffd1ea98f0fa
ujlm <maesulysse@gmail.com>
2023-03-03 19:42:14 +0100

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from transformers import pipeline
 import torch
 import whisper_timestamped as whisper
+import json
+import requests
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
@@ -24,5 +26,25 @@ def inference(model_inputs:dict) -> dict:
     audio = whisper.load_audio(prompt["url"])
     result = whisper.transcribe(model, audio)
 
+    # HTTP callback to api to confirm result
+    response = callback(result)
+
     # Return the results as a dictionary
     return result
+
+
+def callback(result: dict):
+    # HTTP callback to api to confirm result
+    # define the URL to send the request to
+    url = "http://localhost:3000/api/testEmail"
+
+    # convert the data to JSON format
+    json_result = json.dumps(result)
+
+    # define the headers for the request
+    headers = {"Content-Type": "application/json"}
+
+    # send the POST request with the JSON payload
+    response = requests.post(url, data=json_result, headers=headers)
+    
+    return response
