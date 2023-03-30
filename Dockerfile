63a51899c7f5233baa7a4055a386cd62078997a5
ujlm <maesulysse@gmail.com>
2023-03-30 13:59:29 +0200

    update file

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,6 @@ RUN apt-get update && apt-get install -y git
 RUN pip3 install --upgrade pip
 ADD requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
-RUN pip3 install git+https://github.com/linto-ai/whisper-timestamped
 
 # We add the banana boilerplate here
 ADD server.py .
--- app.py
+++ app.py
@@ -1,50 +1,43 @@
-from transformers import pipeline
 import torch
-import whisper_timestamped as whisper
-import json
 import requests
+import whisper
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
-    
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = whisper.load_model("medium", device=device)
 
+def callback(result):
+    url = 'https://scribewave.com/api/whisper/banana/callback'
+    headers = {'Content-type': 'application/json'}
+    data = {
+        "projectId": "random",
+        "transcribeResult": result,
+        "secret": "curieuze5neuze8mosterdpot33"
+    }
+    response = requests.post(url, json=data, headers=headers)
+    print(response.json())
+
+
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
-def inference(model_inputs:dict) -> dict:
+def inference(model_inputs: dict) -> dict:
     global model
 
     # Parse out your arguments
     prompt = model_inputs.get('prompt', None)
     if prompt == None:
         return {'message': "No prompt provided"}
-    
+
     # Run the model
     audio = whisper.load_audio(prompt["url"])
     result = whisper.transcribe(model, audio)
 
-    # HTTP callback to api to confirm result
-    response = callback(result)
+    # Callback
+    callback(result)
 
     # Return the results as a dictionary
     return result
-
-
-def callback(result: dict):
-    # HTTP callback to api to confirm result
-    # define the URL to send the request to
-    url = "http://localhost:3000/api/testEmail"
-
-    # convert the data to JSON format
-    json_result = json.dumps(result)
-
-    # define the headers for the request
-    headers = {"Content-Type": "application/json"}
-
-    # send the POST request with the JSON payload
-    response = requests.post(url, data=json_result, headers=headers)
-    
-    return response
